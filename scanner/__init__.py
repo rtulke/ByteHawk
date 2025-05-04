79b3a09693727f75c03f131233f4386a88f536b2
Robert Tulke <rt@debian.sh>
2025-05-04 20:05:10 +0200

    Update __init__.py

--- scanner/__init__.py
+++ scanner/__init__.py
@@ -8,4 +8,5 @@ Initialisierungsfile für das Scanner-Modul.
 from scanner.base_scanner import FormatScanner
 from scanner.png_scanner import PNGScanner
 from scanner.jpeg_scanner import JPEGScanner
+from scanner.gif_scanner import GIFScanner
 # Weitere Scanner importieren, wenn implementiert
