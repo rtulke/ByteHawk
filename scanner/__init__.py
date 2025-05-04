dc53190591561af8ece9f1c0d5e767733dbe52f5
Robert Tulke <rt@debian.sh>
2025-05-04 19:47:09 +0200

    Create __init__.py

---
+++ scanner/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+scanner/__init__.py
+
+Initialisierungsfile für das Scanner-Modul.
+"""
+
+from scanner.base_scanner import FormatScanner
+from scanner.png_scanner import PNGScanner
+from scanner.jpeg_scanner import JPEGScanner
+# Weitere Scanner importieren, wenn implementiert
