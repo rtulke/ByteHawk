c0c0d527ff3d11a08b83ba2493b514e7365f7e43
Robert Tulke <rt@debian.sh>
2025-05-04 19:47:42 +0200

    Create __init__.py

---
+++ utils/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+utils/__init__.py
+
+Initialisierungsfile für das Utils-Modul.
+"""
+
+from utils.format_definitions import FormatDefinitions
+from utils.payload_analyzer import PayloadAnalyzer
+from utils.helpers import format_size, is_binary_file, print_results
