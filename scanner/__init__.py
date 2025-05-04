17f6b8f5a5b9b9ef4501b4e1d5c666f3d44a0007
Robert Tulke <rt@debian.sh>
2025-05-04 20:24:36 +0200

    Update __init__.py

--- scanner/__init__.py
+++ scanner/__init__.py
@@ -9,4 +9,8 @@ from scanner.base_scanner import FormatScanner
 from scanner.png_scanner import PNGScanner
 from scanner.jpeg_scanner import JPEGScanner
 from scanner.gif_scanner import GIFScanner
+from scanner.pdf_scanner import PDFScanner
+from scanner.office_scanner import OfficeScanner
+from scanner.mp3_scanner import MP3Scanner
+from scanner.pe_elf_scanner import ExecutableScanner
 # Weitere Scanner importieren, wenn implementiert
